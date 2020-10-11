a97c5b6814cc8711e7956197e254d66210bbd5b9
BEUMGOO <captain_kbg@naver.com>
2020-10-11 13:31:51 +0900

    add socketio

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { join } from "path";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -13,4 +15,16 @@ const handleListening = () => console.log(`✅ Server running Port: ${PORT}`);
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotifi", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,19 @@
-hello
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+const socket = io("/");
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`)
+}
+
+function setNickName(nickname) {
+  socket.emit("setNickname", { nickname })
+}
+
+function handleMessageNotification(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotifi", handleMessageNotification)
